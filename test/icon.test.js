7f5d65017e69d702f67a65fccc733d376257c22e
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-01-28 15:25:56 -0500

    update tests for Icon to account for Text, update Icon

--- test/icon.test.js
+++ test/icon.test.js
@@ -1,5 +1,6 @@
 const Icon = require("../lib/icon.js");
 const {Circle} = require("../lib/shapes.js");
+const Text = require("../lib/text.js");
 
 describe("Icon", () => {
     describe("Icon()", () => {
@@ -15,13 +16,22 @@ describe("Icon", () => {
             const icon = new Icon(shape);
             expect(icon.shape).toEqual(shape);
         });
+
+        // Test that text can be assigned
+        it("should store its text property", () => {
+            const shape = new Circle();
+            const text = new Text();
+            const icon = new Icon(shape, text);
+            expect(icon.text).toEqual(text);
+        });
     });
 
     describe("render()", () => {
         it("should return the correct SVG string", () => {
             const shape = new Circle();
-            const icon = new Icon(shape);
-            const expected = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render()}<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text></svg>`;
+            const text = new Text();
+            const icon = new Icon(shape, text);
+            const expected = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render() + text.render()}</svg>`;
             expect(icon.render()).toEqual(expected);
         })
     })
