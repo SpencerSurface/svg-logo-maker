52a72007f5ed5f23967c83665c03cf3e67b8e2e7
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-01-28 15:19:25 -0500

    add test for Text's render() method, implement method

--- test/text.test.js
+++ test/text.test.js
@@ -10,16 +10,27 @@ describe("Text", () => {
 
         // Test that text content can be assigned
         it("should store its content property", () => {
-            const textContent = "JS";
-            const text = new Text(textContent);
-            expect(text.content).toEqual(textContent);
-        })
+            const content = "JS";
+            const text = new Text(content);
+            expect(text.content).toEqual(content);
+        });
 
         // Test that text color can be assigned
         it("should store its color property", () => {
-            const textColor = "chartreuse";
-            const text = new Text("JS", "chartreuse");
-            expect(text.color).toEqual(textColor);
-        })
+            const color = "chartreuse";
+            const text = new Text("JS", color);
+            expect(text.color).toEqual(color);
+        });
+    });
+
+    describe("render()", () => {
+        // Test that the render() method returns the correct SVG string
+        it("should return the correct SVG string", () => {
+            const content = "CSS";
+            const color = "#FF00FF"
+            const text = new Text(content, color);
+            const expected = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${content}</text>`;
+            expect(text.render()).toEqual(expected);
+        });
     });
 });
