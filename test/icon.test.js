68665d7b3e6eab49a0732516c33c900b41b5db63
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-01-28 15:03:45 -0500

    add test for Icon's render() method, implement said method

--- test/icon.test.js
+++ test/icon.test.js
@@ -1,5 +1,5 @@
 const Icon = require("../lib/icon.js");
-const {Shape} = require("../lib/shapes.js");
+const {Circle} = require("../lib/shapes.js");
 
 describe("Icon", () => {
     describe("Icon()", () => {
@@ -11,9 +11,18 @@ describe("Icon", () => {
 
         // Test that shape can be assigned
         it("should store its shape property", () => {
-            const shape = new Shape();
+            const shape = new Circle();
             const icon = new Icon(shape);
             expect(icon.shape).toEqual(shape);
-        })
+        });
     });
+
+    describe("render()", () => {
+        it("should return the correct SVG string", () => {
+            const shape = new Circle();
+            const icon = new Icon(shape);
+            const expected = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shape.render()}<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text></svg>`;
+            expect(icon.render()).toEqual(expected);
+        })
+    })
 });
