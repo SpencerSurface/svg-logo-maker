d5cee701a4f5a8a212acdd8348800af19017a17b
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-01-28 12:22:46 -0500

    add tests for Circle, add more tests for Shape

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -3,7 +3,7 @@ const {Shape, Circle, Square, Triangle} = require("../lib/shapes.js");
 describe("Shape", () => {
     describe("Shape()", () => {
         // Test that the Shape class can be instantiated
-        it("should initialize correctly", () => {
+        it("should be an instance of Shape", () => {
             const shape = new Shape();
             expect(shape).toBeInstanceOf(Shape);
         });
@@ -14,6 +14,23 @@ describe("Shape", () => {
             const shape = new Shape(color);
             expect(shape.color).toEqual(color);
         });
+
+        // Test that text can be assigned
+        it("should store its text property", () => {
+            const color = "chartreuse";
+            const text = "JS";
+            const shape = new Shape(color, text);
+            expect(shape.text).toEqual(text);
+        });
+
+        // Test that textColor can be assigned
+        it("should store its text property", () => {
+            const color = "chartreuse";
+            const text = "JS";
+            const textColor = "#000000"
+            const shape = new Shape(color, text, textColor);
+            expect(shape.textColor).toEqual(textColor);
+        });
     });
 
     describe("render()", () => {
@@ -22,6 +39,34 @@ describe("Shape", () => {
             const shape = new Shape();
             const error = new Error("render() must be overriden in Shape's subclasses");
             expect(() => shape.render()).toThrow(error);
+        });
+    });
+});
+
+describe("Circle", () => {
+    describe("Circle()", () => {
+        // Test that the Circle class can be instantiated
+        it("should be an instance of Circle", () => {
+            const circle = new Circle();
+            expect(circle).toBeInstanceOf(Circle);
+        });
+
+        // Test that the Circle class is a subclass of the Shape class
+        it("should be an instance of Shape", () => {
+            const circle = new Circle();
+            expect(circle).toBeInstanceOf(Shape);
+        });
+    });
+
+    describe("render()", () => {
+        // Test that returns the correct SVG string
+        it("should return an SVG string", () => {
+            const color = "chartreuse";
+            const text = "JS";
+            const textColor = "#000000";
+            const circle = new Circle(color, text, textColor);
+            const expected = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text></svg>`;
+            expect(circle.render()).toEqual(expected);
         });
     });
 });
