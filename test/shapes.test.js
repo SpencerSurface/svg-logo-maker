7225b9d9d027ecff6b4745955883598339ac0371
Spencer Surface <54449845+SpencerSurface@users.noreply.github.com>
2024-01-28 14:40:49 -0500

    update tests

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -10,26 +10,9 @@ describe("Shape", () => {
 
         // Test that color can be assigned
         it("should store its color property", () => {
-            const color = "chartreuse";
+            const color = "#000000";
             const shape = new Shape(color);
             expect(shape.color).toEqual(color);
-        });
-
-        // Test that text can be assigned
-        it("should store its text property", () => {
-            const color = "chartreuse";
-            const text = "JS";
-            const shape = new Shape(color, text);
-            expect(shape.text).toEqual(text);
-        });
-
-        // Test that textColor can be assigned
-        it("should store its textColor property", () => {
-            const color = "chartreuse";
-            const text = "JS";
-            const textColor = "#000000"
-            const shape = new Shape(color, text, textColor);
-            expect(shape.textColor).toEqual(textColor);
         });
     });
 
@@ -62,10 +45,8 @@ describe("Circle", () => {
         // Test that render() returns the correct SVG string
         it("should return correct SVG string", () => {
             const color = "chartreuse";
-            const text = "JS";
-            const textColor = "#000000";
-            const circle = new Circle(color, text, textColor);
-            const expected = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text></svg>`;
+            const circle = new Circle(color);
+            const expected = `<circle cx="150" cy="100" r="80" fill="${color}" />`;
             expect(circle.render()).toEqual(expected);
         });
     });
@@ -90,10 +71,8 @@ describe("Square", () => {
         // Test that render() returns the correct SVG string
         it("should return correct SVG string", () => {
             const color = "chartreuse";
-            const text = "JS";
-            const textColor = "#000000";
-            const square = new Square(color, text, textColor);
-            const expected = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><square x="70" y="20" width="160" height="160" fill="${color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text></svg>`;
+            const square = new Square(color);
+            const expected = `<square x="70" y="20" width="160" height="160" fill="${color}" />`;
             expect(square.render()).toEqual(expected);
         });
     });
@@ -116,13 +95,11 @@ describe("Triangle", () => {
 
     describe("render()", () => {
         // Test that render() returns the correct SVG string
-        it("should return correct SVG string", () => {
-            const color = "chartreuse";
-            const text = "JS";
-            const textColor = "#000000";
-            const triangle = new Triangle(color, text, textColor);
-            const expected = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="58,20 242,20 150,180" fill="${color}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text></svg>`;
-            expect(triangle.render()).toEqual(expected);
+        // (Required test)
+        it("should return the correct SVG string", () => {
+            const shape = new Triangle();
+            shape.setColor("blue");
+            expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
         });
     });
 });
